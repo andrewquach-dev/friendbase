2bce803a99fc57084d4c5d1b26b99744ed538e8c
Andrew Quach <andrewrquach@gmail.com>
2023-03-02 17:30:00 -0800

    changed comment model to have id of user for showing username

--- models/Comment.js
+++ models/Comment.js
@@ -13,6 +13,10 @@ const CommentSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Post",
   },
+  createdById: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
   createdAt: {
     type: Date,
     default: Date.now,
